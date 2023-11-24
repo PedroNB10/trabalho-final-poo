34e99aae08cf2f496275715cb1427914fa31a988
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-11-23 23:33:01 -0300

    ajuste funcionalidade consulta

--- nota.py
+++ nota.py
@@ -36,16 +36,49 @@ class ControleNota:
 
     def lista_de_notas_fiscais_geradas(self):
         return self.__lista_de_notas_fiscais
+    
+    def criar_janela_consultar_nota(self, event):
+        self.__limite_consultar_nota = LimiteConsultarNota(self)
 
+    def consultar_nota_handler(self, event):
+        cpf = self.__limite_consultar_nota.input_cpf.get()
+        clientes = self.__controle_principal.controle_cliente.lista_de_clientes_cadastrados()
+        for cliente in clientes:
+            if cliente.cpf == cpf:
+                notas = cliente.lista_de_notas
+                for nota in notas:
+                    if nota.cpf_cliente == cpf:
+                        messagebox.showinfo("Nota Fiscal", "Número da Nota: " + str(nota.numero) + "\nData: " + nota.data + "\nCPF do Cliente: " + nota.cpf_cliente)
+                        break
+                break
+        messagebox.showinfo("Nota Fiscal", "Não há notas fiscais para este cliente")
     def fechar_janela(self, janela):
         janela.destroy()
     
+    
+class LimiteConsultarNota(tk.Toplevel):
+    def __init__(self, controle):
+        self.controle = controle
+        tk.Toplevel.__init__(self)
+        self.title("Consultar Nota Fiscal")
+        self.geometry('300x100')
+        self.frame_nota = tk.Frame(self)
+        self.label_nota = tk.Label(self.frame_nota, text="Digite o número do CPF: ")
+        self.label_nota.pack(side="left")
+        self.input_cpf = tk.Entry(self.frame_nota, width=20)
+        self.input_cpf.pack(side="left")
+        self.frame_nota.pack()
+        self.botao_consultar = tk.Button(self, text="Consultar")
+        self.botao_consultar.pack()
+        self.botao_consultar.bind("<Button>", controle.consultar_nota_handler)
 
+
+    
 class JanelaAuxiliar:
-    def __init__(self, controller):
+    def __init__(self, controle):
         self.raiz = tk.Tk()
         self.raiz.title("janela Auxiliar")
-        self.controller = controller
+        self.controle = controle
         self.frame_height = 400
         self.frame_width = 400
         self.screen_width = self.raiz.winfo_screenwidth()
@@ -57,7 +90,7 @@ class JanelaAuxiliar:
 
         self.bt1 = tk.Button(self.raiz, text="Consultar Nota Fiscal")
         self.bt1.pack()
-        # self.btn.bind("<Button>", controle.enterHandler)
+        self.bt1.bind("<Button>", controle.criar_janela_consultar_nota)
         
         self.bt2 = tk.Button(self.raiz, text="Consultar Vendas por Cliente")
         self.bt2.pack()
@@ -92,6 +125,8 @@ class JanelaAuxiliar:
 
 
         self.raiz.mainloop()
+
+    
 
         
     
